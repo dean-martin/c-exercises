4bb4a7411fc9dc2f7708e4e3101b18b7edd6a416
Dean Martin <contact@deanmartin.pro>
2025-01-04 10:46:46 -0500

    7-01

---
+++ src/7-01.c
@@ -0,0 +1,20 @@
+// Exercise 7-1.
+// Write a program that converts upper case to lower or lower case
+// to upper, depending on the name it is invoked with, as found in argv[0].
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int c;
+
+    if (strstr(argv[0], "toupper"))
+        while ((c = getchar()) != EOF)
+            putchar(toupper(c));
+    else
+        while ((c = getchar()) != EOF)
+            putchar(tolower(c));
+    return 0;
+}
